227f0982527964bf9ea062fe846d1c725769de8c
Deep Patel <19it091@charusat.edu.in>
2021-10-20 10:49:56 +0530

    Manually Location

--- lib/Screens/location_screen.dart
+++ lib/Screens/location_screen.dart
@@ -1,8 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:csc_picker/csc_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:free_book/Screens/home_screen.dart';
-import 'package:free_book/Screens/login_screen.dart';
 import 'package:location/location.dart';
 
 class LocationScreen extends StatefulWidget {
@@ -13,15 +12,19 @@ class LocationScreen extends StatefulWidget {
 }
 
 class _LocationScreenState extends State<LocationScreen> {
-
-  bool _loading=false;
+  bool _loading = false;
   Location location = new Location();
+
+  String countryValue = "";
+  String stateValue = "";
+  String cityValue = "";
+  String address = "";
 
   late bool _serviceEnabled;
   late PermissionStatus _permissionGranted;
   late LocationData _locationData;
-  
-  Future<LocationData?>getLocation() async {
+
+  Future<LocationData?> getLocation() async {
     _serviceEnabled = await location.serviceEnabled();
     if (!_serviceEnabled) {
       _serviceEnabled = await location.requestService();
@@ -42,15 +45,127 @@ class _LocationScreenState extends State<LocationScreen> {
 
     print(_locationData);
 
-    
     return _locationData;
   }
 
- 
-
   @override
   Widget build(BuildContext context) {
+    showBottomScreen(context) {
+      showModalBottomSheet(
+          isScrollControlled: true,
+          enableDrag: true,
+          context: context,
+          builder: (context) {
+            return Column(
+              children: [
+                SizedBox(
+                  height: 26,
+                ),
+                AppBar(
+                  automaticallyImplyLeading: false,
+                  iconTheme: IconThemeData(color: Colors.black),
+                  elevation: 1,
+                  backgroundColor: Colors.white,
+                  title: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: Icon(Icons.clear),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        'Location',
+                        style: TextStyle(color: Colors.black),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    child: SizedBox(
+                      height: 40,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Search City,area or neighbourhood',
+                          hintStyle: TextStyle(color: Colors.grey),
+                          icon: Icon(Icons.search),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                ListTile(
+                  onTap: () {},
+                  horizontalTitleGap: 0.0,
+                  leading: Icon(
+                    Icons.my_location,
+                    color: Colors.blue,
+                  ),
+                  title: Text(
+                    'Use current location',
+                    style: TextStyle(
+                        color: Colors.blue, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(
+                    'Enable location',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.grey.shade300,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10, bottom: 4, top: 4),
+                    child: Text(
+                      'CHOOSE CITY',
+                      style: TextStyle(
+                          color: Colors.blueGrey.shade900, fontSize: 12),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: CSCPicker(
+                    layout: Layout.vertical,
+                    dropdownDecoration: BoxDecoration(shape: BoxShape.rectangle),
+                    defaultCountry: DefaultCountry.India,
+                    onCountryChanged: (value) {
+                      setState(() {
+                        countryValue = value;
+                      });
+                    },
+                    onStateChanged: (value) {
+                      setState(() {
+                        stateValue = value!;
+                      });
+                    },
+                    onCityChanged: (value) {
+                      setState(() {
+                        cityValue = value!;
+                        address = '$cityValue,$stateValue,${countryValue.substring(8)}';
+                      });
+                      print(address);
+
+                    },
+                  ),
+                )
+              ],
+            );
+          });
+    }
+
     return Scaffold(
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomInset: false,
       body: Column(
         children: [
           Image.asset('images/map.jpg'),
@@ -79,45 +194,61 @@ class _LocationScreenState extends State<LocationScreen> {
               child: Row(
                 children: [
                   Expanded(
-                    child: _loading?Center(child: CircularProgressIndicator(
-                      valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor),
-                    )):ElevatedButton.icon(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            Theme.of(context).primaryColor),
-                      ),
-                      icon: Icon(CupertinoIcons.location_fill),
-                      label: Padding(
-                        padding: const EdgeInsets.only(top: 15, bottom: 15),
-                        child: Text(
-                          'Around me',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      onPressed:(){
-                        setState(() {
-                          _loading=true;
-                        });
-                        getLocation().then((value){
-                          print(_locationData.latitude);
-                          if(value!=null){
-                            Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => HomeScreen(locationData: _locationData),),);
-                          }
-                        });
-                      },
-                    ),
+                    child: _loading
+                        ? Center(
+                            child: CircularProgressIndicator(
+                            valueColor: AlwaysStoppedAnimation<Color>(
+                                Theme.of(context).primaryColor),
+                          ))
+                        : ElevatedButton.icon(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  Theme.of(context).primaryColor),
+                            ),
+                            icon: Icon(CupertinoIcons.location_fill),
+                            label: Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 15, bottom: 15),
+                              child: Text(
+                                'Around me',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _loading = true;
+                              });
+                              getLocation().then((value) {
+                                print(_locationData.latitude);
+                                if (value != null) {
+                                  Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (BuildContext context) =>
+                                          HomeScreen(
+                                              locationData: _locationData),
+                                    ),
+                                  );
+                                }
+                              });
+                            },
+                          ),
                   ),
                 ],
               )),
-          TextButton(
-            onPressed: () {  },
-            child: Text(
-              'Set location manually',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-                fontSize: 18,
-                decoration: TextDecoration.underline,
+          InkWell(
+            onTap: () {
+              showBottomScreen(context);
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration:
+                    BoxDecoration(border: Border(bottom: BorderSide(width: 2))),
+                child: Text(
+                  'Set Location manually',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
               ),
             ),
           )
@@ -126,10 +257,3 @@ class _LocationScreenState extends State<LocationScreen> {
     );
   }
 }
-
-
-
-
-
-
-
